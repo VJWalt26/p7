6e6db5f77e3f6758114cc910bc32c43a5efe6f77
Virginia <virginia.hoskins@gmail.com>
2015-08-05 16:40:32 -0600

    changes program 7

--- p7.js
+++ p7.js
@@ -1,4 +1,3 @@
-alert("is this working");
 
 setClickHandler("clickRoll",rollDice);
 
@@ -10,14 +9,68 @@ var diceRoll = {
  	5: "&#x2684",
  	6: "&#x2685"
  };
- //setHTML("diceGoHere" , diceRoll[Math.floor(Math.random() * 6 ) + 1]);
- //setHTML("diceGoHere" , Math.floor(Math.random() * 6) + 1);
+ var Die = function () {
+		this.value=0;	
+	};
+ Die.prototype.roll = function(){
+ 	this.value=Math.floor(Math.random() * 6) + 1;
 
-function rollDice(){
-	var getValue = diceRoll[Math.floor(Math.random() * 6) + 1];
-	setHTML("diceGoHere" , getValue);
- 	
+ }
+ Die.prototype.getValue = function(){
+ 	return this.value;
+ }
+ Die.prototype.getHTML = function() {
+ 	return diceRoll[this.value];
+ }
  
- //var die = 
+function rollDice(){
+	var diceNum = (getInput("diceInput"));
+	var total = 0;
+	var text = " ";
+	if (isNaN(diceNum)){
+		alert("That is not a number");
+	}
+	else if (diceNum > 20){
+		alert("Please choose a number between 0 and 20");
+	}
+	else if (diceNum < 0){
+		alert("Please choose a number between 0 and 20");
+	}
+	else {
 
+    var diceArray = [];
+    for (i=0; i<diceNum; i++){
+    	diceArray.push(new Die());
+
+
+	}
+	for (i=0;i<diceNum; i++){
+		diceArray[i].roll();
+
+
+	}
+	for (i=0;i<diceNum;i++){
+		total += diceArray[i].getValue();
+
+
+	}
+	for (i=0;i<diceNum;i++){
+		text += diceArray[i].getHTML();
+
+	}
+
+
+	setHTML("Total" , total);
+	setHTML("diceGoHere",text);
+
+	}
 }
+
+	
+
+
+
+		
+
+	
+
